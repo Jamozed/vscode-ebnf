eaee3b012e7526d852ff0c623289978886457f3a
Jamozed <jake@omkov.net>
2021-07-06 21:25:32 +1200

    Add foundations for programmatic language features

---
+++ src/extension.ts
@@ -0,0 +1,9 @@
+// estension.ts
+// Copyright (C) 2021, Jakob Wakeling
+// All rights reserved.
+
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+	console.log("EBNF for Visual Studio Code activated");
+}
